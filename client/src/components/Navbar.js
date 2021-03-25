cc3473ede339c14593f844f3369d46c09dcc32d4
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-03-25 17:58:01 +0000

    navbar search function

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from 'react-select'
+import { continentOptions, suitableOptions, tagOptions } from './data/searchData'
+
+const groupedOptions = [
+  { label: 'Continents', options: continentOptions },
+  { label: 'Suitable For', options: suitableOptions },
+  { label: 'Tags', options: tagOptions }
+]
 
 const Navbar = () => {
+
+  const [formdata, setFormdata] = useState({
+    search: []
+  })
+
+  const handleMultiChange = (selected, name) => {
+    const values = selected ? selected.map(item => item.value) : []
+    setFormdata({ ...formdata, [name]: [...values] })
+  }
+
   return (
     <div>
-      
+      <div className="control">
+        <Select
+          options={groupedOptions}
+          isMulti
+          name="search"
+          onChange={(selected) => handleMultiChange(selected, 'search')}
+        />
+      </div>
     </div>
   )
 }
