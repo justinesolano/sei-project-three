3ccd2694e31f5b4d16ddac299d0acdc3c00c65d1
jsolano <justinesolano2370@gmail.com>
2021-03-25 18:01:50 +0000

    navbar aligning breakthrough

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,33 +4,37 @@ import jetflixLogo from '../assets/jetflixlogo.png'
 
 const Navbar = () => {
   return (
-    <nav className="ui grid fixed menu" style={{ width: '100vw' }}>
+    <nav className="ui menu" style={{ width: '100vw' }}>
       <div className="nav-container">
-        <div className="jetflix-nav-logo">
-          <Link to="/" className="logo-to-home">
-            <img src={jetflixLogo}></img>
-          </Link>
-        </div>
-        <div className="my-list">
-          <Link to="/" className="my-list-link">
-            <a className="nav-links">My List</a>
-          </Link>
-        </div>
-        <div className="explore">
-          <Link to="/profiles" className="explore-link">
-            <a className="nav-links">Explore</a>
-          </Link>
-        </div>
-        <div className="profiles" textAlign='right'>
-          <Link to="/profiles" className="profiles-link">
-            <a className="nav-links">Search</a>
-          </Link>
-        </div>
-        <div className="profiles">
-          <Link to="/profiles" className="profiles-link">
-            <a className="nav-links">Profiles</a>
-          </Link>
-        </div>
+        <section className="left-nav">
+          <div className="jetflix-nav-logo">
+            <Link to="/home" className="logo-to-home">
+              <img src={jetflixLogo}></img>
+            </Link>
+          </div>
+          <div className="my-list">
+            <Link to="/home" className="my-list-link">
+              <a className="nav-links">My List</a>
+            </Link>
+          </div>
+          <div className="explore">
+            <Link to="/profiles" className="explore-link">
+              <a className="nav-links">Explore</a>
+            </Link>
+          </div>
+        </section>
+        <section className="right-nav">
+          <div className="profiles" textAlign='right'>
+            <Link to="/profiles" className="profiles-link">
+              <a className="nav-links">Search</a>
+            </Link>
+          </div>
+          <div className="profiles">
+            <Link to="/profiles" className="profiles-link">
+              <a className="nav-links">Profiles</a>
+            </Link>
+          </div>
+        </section>
       </div>
     </nav>
   )
