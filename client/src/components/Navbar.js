e83201ba47228c34641a851e2e2cab4937ad268d
jsolano <justinesolano2370@gmail.com>
2021-03-25 17:16:06 +0000

    navbar changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import jetflixLogo from '../assets/jetflixlogo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="ui grid fixed menu" style={{ width: '100vw' }}>
+      <div className="nav-container">
+        <div className="jetflix-nav-logo">
+          <Link to="/" className="logo-to-home">
+            <img src={jetflixLogo}></img>
+          </Link>
+        </div>
+        <div className="my-list">
+          <Link to="/" className="my-list-link">
+            <a className="nav-links">My List</a>
+          </Link>
+        </div>
+        <div className="explore">
+          <Link to="/profiles" className="explore-link">
+            <a className="nav-links">Explore</a>
+          </Link>
+        </div>
+        <div className="profiles" textAlign='right'>
+          <Link to="/profiles" className="profiles-link">
+            <a className="nav-links">Search</a>
+          </Link>
+        </div>
+        <div className="profiles">
+          <Link to="/profiles" className="profiles-link">
+            <a className="nav-links">Profiles</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
   )
 }
+
+// home
+// my list
+// explore - link to explore page into div
+
+// div
+// search
+// profile - image 
 
 export default Navbar
