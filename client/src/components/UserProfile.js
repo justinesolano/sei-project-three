111fdc5549502b7c3bf7c8286d8fa09fee3b863b
olilewis1 <olilewis1@hotmail.co.uk>
2021-03-26 14:30:56 +0000

    added pictutres to profile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const UserProfile = () => {
+  const { id } = useParams()
+  const [profile, setProfile] = useState(null) 
+
+  useEffect(() => { 
+    const getData = async () => { 
+      const response = await axios.get(`/api/profiles/${id}`)
+      setProfile(response.data)
+    }
+    getData()
+  }, [] ) 
+  console.log(profile)
+
+  if (!profile) return null 
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        {profile.username}
+      </div>
+      {profile.photos.map(photo => ( 
+        <div  key={photo.title}> 
+          <img key={photo.id} className='photo-userprofile' src={photo.image} alt={photo.title} /> 
+        </div>
+      ) )}
+    </>
   )
 }
 
