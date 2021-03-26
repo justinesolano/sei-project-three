3996a8bfb37026eee55c8118ab8ccf8add5d42c9
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-03-26 16:42:41 +0000

    carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
+import Slider from 'react-slick'
 
 const Home = () => {
 
@@ -20,6 +21,35 @@ const Home = () => {
   useEffect(() => {
     (!destinations) ? setHero(0) : setHero(parseFloat(Math.floor(Math.random() * destinations.length)))
   })
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      }
+    ]
+  }
 
   console.log(destinations)
 
@@ -41,47 +71,44 @@ const Home = () => {
         </div>
       </div>
       <div className="previews">
-        <div>
-          <h3 className="ui home-subheader">My List</h3>
-          <div className="ui grid">
-            <div className="four column row">
-              {destinations.map(destination => {
-                return (
-                  <Link to={`/destinations/${destination.id}`} key={destination.id} className="home-column column home-container">
-                    <img src={destination.image} className="home-item"/>
-                  </Link>
-                )
-              })}
-            </div>
-          </div>
+        <h3 className="ui home-subheader">My List</h3>
+        <div className="home-container">
+          <Slider {...settings} className="slider">
+            {destinations.map(destination => {
+              return <div key={destination._id} className="home-item">
+                <img src={destination.image} 
+                  style={{
+                    'width': '500px'
+                  }}/>
+              </div>
+            })}
+          </Slider>
         </div>
-        <div>
-          <h3 className="ui home-subheader">Recommended for you</h3>
-          <div className="ui grid">
-            <div className="four column row">
-              {destinations.map(destination => {
-                return (
-                  <Link to={`/destinations/${destination.id}`} key={destination.id} className="home-column column home-container">
-                    <img src={destination.image} className="home-item"/>
-                  </Link>
-                )
-              })}
-            </div>
-          </div>
+        <h3 className="ui home-subheader">Recommended for you</h3>
+        <div className="home-container">
+          <Slider {...settings} className="slider">
+            {destinations.map(destination => {
+              return <div key={destination._id} className="home-item">
+                <img src={destination.image} 
+                  style={{
+                    'width': '500px'
+                  }}/>
+              </div>
+            })}
+          </Slider>
         </div>
-        <div>
-          <h3 className="ui home-subheader">Trending Now</h3>
-          <div className="ui grid">
-            <div className="four column row">
-              {destinations.map(destination => {
-                return (
-                  <Link to={`/destinations/${destination.id}`} key={destination.id} className="home-column column home-container">
-                    <img src={destination.image} className="home-item"/>
-                  </Link>
-                )
-              })}
-            </div>
-          </div>
+        <h3 className="ui home-subheader">Trending now</h3>
+        <div className="home-container">
+          <Slider {...settings} className="slider">
+            {destinations.map(destination => {
+              return <div key={destination._id} className="home-item">
+                <img src={destination.image} 
+                  style={{
+                    'width': '500px'
+                  }}/>
+              </div>
+            })}
+          </Slider>
         </div>
       </div>
     </div>
