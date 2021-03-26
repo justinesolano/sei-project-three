6365459d099d16841e9f307165d5c1a104a74571
olilewis1 <olilewis1@hotmail.co.uk>
2021-03-26 16:45:59 +0000

    added a map

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,31 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import ReactMapGL from 'react-map-gl'
 
 const UserProfile = () => {
-  const { id } = useParams()
-  const [profile, setProfile] = useState(null) 
 
-  useEffect(() => { 
-    const getData = async () => { 
+
+  console.log('my token ', process.env.REACT_APP_MAPBOX_ACCESS_TOKEN)
+  const { id } = useParams()
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    const getData = async () => {
       const response = await axios.get(`/api/profiles/${id}`)
       setProfile(response.data)
     }
     getData()
-  }, [] ) 
-  console.log(profile)
+  }, [])
 
-  if (!profile) return null 
+
+
+  if (!profile) return null
   return (
     <>
-      <div>
-        {profile.username}
+      {/* <div>
+        <h1>{profile.username} </h1>
+      </div> */}
+      <div className='map-container'>
+        <ReactMapGL
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          height='50%'
+          width='50%'
+          mapStyle='mapbox://styles/mapbox/streets-v11'
+          latitude={51.515}
+          longitude={-0.078}
+          zoom={10}
+        >
+
+        </ReactMapGL>
       </div>
-      {profile.photos.map(photo => ( 
-        <div  key={photo.title}> 
-          <img key={photo.id} className='photo-userprofile' src={photo.image} alt={photo.title} /> 
+      {/* {profile.photos.map(photo => (
+        <div key={photo.title}>
+          <h3>{photo.title} </h3>
+          <img key={photo.id} className='photo-userprofile' src={photo.image} alt={photo.title} />
         </div>
-      ) )}
+      ))} */}
     </>
   )
 }
