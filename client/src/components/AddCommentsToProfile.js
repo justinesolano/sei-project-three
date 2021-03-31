b1928b81af4ecc967ddf240011df56a8f5893e37
olilewis1 <olilewis1@hotmail.co.uk>
2021-03-31 15:13:20 +0100

    branch checking

--- client/src/components/AddCommentsToProfile.js
+++ client/src/components/AddCommentsToProfile.js
@@ -16,59 +16,83 @@ const AddCommentsToProfile = () => {
     }
     getData()
   }, [])
+
+  const [eventName, setEventName] = useState('')
+  const [FormData] = useState({
+    like: true
+  })
+  const handleLike = event => {
+    console.log(event.target.name, eventName)
+    setEventName(event.target.name)
+    const setLikes = async () => {
+      const token = window.localStorage.getItem('token')
+      await axios.post(`/api/profiles/${id}/photos/${event.target.name}/likes`, FormData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+      window.location.reload()
+    }
+    setLikes()
+  }
   if (!profile) return null
   return (
-    <div className='card columns is-multiline show-comments is-vcentered'>
-      {profile.photos.map(photo => {
-        return (
-          <>
-            <div className='show-comments'>
-              <div className='card-image card-width column'>
-                <div>
-                  <figure className='image'>
-                    <img src={photo.image} alt='Placeholder' />
-                  </figure>
-                </div>
-                <div className='card-content'>
-                  <div className='media'>
-                    <div className='media-left'>
-                      <figure className='image is-48x48'>
-                        <img src={smileyGreen}alt='Placeholder image'/>
-                      </figure>
+    <div className='body-show-comments'>
+      <div className='card columns is-multiline show-comments is-vcentered'>
+        {profile.photos.map(photo => {
+          return (
+            <>
+              <div className='show-comments'>
+                <div className='card-image card-width column'>
+                  <div>
+                    <figure className='image'>
+                      <img src={photo.image} alt='Placeholder' />
+                    </figure>
+                  </div>
+                  <div className='card-content'>
+                    <div className='media'>
+                      <div className='media-left'>
+                        <figure className='image is-48x48'>
+                          <img src={smileyGreen} alt='Placeholder image' />
+                        </figure>
+                      </div>
+                      <div className='media-content'>
+                        <p className='title is-4'>{photo.title}</p>
+                        <p className='subtitle is-6'>📍 {photo.locationName}</p>
+                      </div>
+                      <button
+                        name={`${photo._id}`}
+                        className='button is-3'
+                        onClick={handleLike}>
+                        <span className='big-heart'> ❤️ </span>&nbsp;  &nbsp; &nbsp;     Likes {photo.likes.length}
+                      </button>
                     </div>
-                    <div className='media-content'>
-                      <p className='title is-4'>{photo.title}</p>
-                      <p className='subtitle is-6'>📍 {photo.locationName}</p>
-                    </div>
-                    <button
-                      name={`${photo._id}`}
-                      className='button is-3'>
-                      <span className='big-heart'> ❤️ </span>&nbsp;  &nbsp; &nbsp;     Likes {photo.likes.length}
-                    </button>
+                  </div>
+                  <div className='content'>
+                    <h5 className='title is-5'>Comments:</h5>
+                    {photo.comments.map(comment => (
+                      <div key={comment._id}>
+                        {
+                          <p className='p-userprofile' >
+                            <Link to={`/profiles/${comment.owner}`}> 👤 - {comment.text} </Link>
+                          </p>}
+                      </div>
+                    ))}
+                    <br>
+                    </br>
+                    <time>11:09 PM - 1 Jan 2016</time>
                   </div>
                 </div>
-                <div className='content'>
-                  <h5 className='title is-5'>Comments:</h5>
-                  {photo.comments.map(comment => (
-                    <div key={comment._id}>
-                      {
-                        <p className='p-userprofile' >
-                          <Link to={`/profiles/${comment.owner}`}> 👤 - {comment.text} </Link>
-                        </p>}
-                    </div>
-                  ))}
-                  <br>
-                  </br>
-                  <time>11:09 PM - 1 Jan 2016</time>
-                </div>
               </div>
-            </div>
-          </>
+            </>
 
-        )
-      })}
+          )
+        })}
 
-    </div >
+      </div >
+    </div>
+
   )
 }
 
