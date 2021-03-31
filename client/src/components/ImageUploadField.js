ac2867ef3ad1c0d15588dc27633de550b435fd6c
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-03-31 17:39:20 +0100

    image uploading fixed and styled

--- client/src/components/ImageUploadField.js
+++ client/src/components/ImageUploadField.js
@@ -1,42 +1,35 @@
-import React, { useState, useCallback } from 'react' 
-import { useDropzone } from 'react-dropzone'
-import styles from '../styles/main.scss'
+import React from 'react'
+import axios from 'axios'
 
 const uploadUrl = process.env.REACT_APP_CLOUDINARY_URL
 const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
 
-export const ImageUploadField = () => {
+export const ImageUploadField = ({ handleImageUrl, value }) => {
 
-  const [uploadedFiles, setUploadedFiles] = useState()
-
-  const onDrop = useCallback((acceptedFiles) => {
-    const url = `https://api.cloudinary.com/v1_1/${uploadUrl}/upload`
-
-    acceptedFiles.forEach(async (acceptedFile) => {
-      const formData = new FormData()
-      formData.append('file', acceptedFile)
-      formData.append('upload_preset', uploadPreset)
-      console.log(formData)
-      const response = await fetch(url, {
-        method: 'post',
-        body: formData
-      })
-      const data = await response.json()
-      setUploadedFiles(data.secure_url)
-    })
-  }, [])
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
-    onDrop, 
-    accepts: 'image/*', 
-    multiple: false 
-  })
-
-  console.log('URL', uploadedFiles)
+  const handleUpload = async event => {
+    const data = new FormData()
+    data.append('file', event.target.files[0])
+    data.append('upload_preset', uploadPreset)
+    const response = await axios.post(uploadUrl, data)
+    handleImageUrl(response.data.url)
+  }
 
   return (
-    <div {...getRootProps()} className={`${styles.dropzone} ${isDragActive ? styles.active : null}`}>
-      <input {...getInputProps()} />
-        Add a picture
-    </div>
+    <>
+      {value ?
+        <div>
+          <img src={value} alt="tiger"/>
+        </div>
+        :
+        <>
+          <label>Profile Image</label>
+          <input
+            className="input"
+            type="file"
+            onChange={handleUpload}
+          />
+        </>
+      }
+    </>
   )
 }
