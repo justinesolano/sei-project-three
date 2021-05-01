da96688a290d3a301bd3ae0aec21e5ea32a5772b
jsolano <justinesolano2370@gmail.com>
2021-05-01 18:40:04 +0100

    managed to get mongo to run finally hope

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,9 +1,9 @@
-export const secret = process.env.SECRET || 'Flynn is flying'
-export const port = process.env.PORT || 4000
-export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/sei-project-three'
+export const secret = 'Flynn is flying'
+export const port = 4000
+export const dbURI = 'mongodb://localhost/sei-project-three'
 
-module.exports = {
-  dbURI,
-  port,
-  secret
-}
+// module.exports = {
+//   dbURI,
+//   port,
+//   secret,
+// }
